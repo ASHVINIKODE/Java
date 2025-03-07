a314a3f1df5d98a1dea713ac55d0ab683760425d
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-03-07 19:41:21 +0530

    initial commit

---
+++ methods.java
@@ -0,0 +1,27 @@
+public class methods {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        swap(a, b);
+        System.out.println(a + " " + b);
+        // it will not swap
+        // passing example
+
+        String name = "Ashvini";
+        changeName(name);
+        System.out.println(name);
+    }
+
+    static void changeName(String naam) {
+        naam = "Ashi";
+
+    }
+
+    static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        // this is excess in this func only not outside the func.
+
+    }
+}
