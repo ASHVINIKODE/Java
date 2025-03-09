ca4f4351d77b8d6c7f0d89c7a0671b70ddfe0c1d
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-03-09 23:06:37 +0530

    initial commit

---
+++ arrays.java
@@ -0,0 +1,11 @@
+public class arrays {
+    public static void main(String[] args) {
+        int[] rollno = new int[5];
+        System.out.println(rollno[1]);
+        System.out.println(rollno[4]);
+        String[] arr = new String[5];
+        System.out.println(arr[1]);
+        System.out.println(arr[3]);
+        // declaration and initialisation of array.
+    }
+}
