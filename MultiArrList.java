21c9e5a6e5084eb3cf0f3eb122cdbd08f0355f2a
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-03-18 23:50:45 +0530

    initial commit

---
+++ MultiArrList.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultiArrList {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        // initialisation
+        for (int i = 0; i < 3; i++) {
+            list.add(new ArrayList<>());
+        }
+        // add elememt
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                list.get(i).add(input.nextInt());
+
+            }
+
+        }
+
+        System.out.println(list);
+        System.out.println();
+
+    }
+}
