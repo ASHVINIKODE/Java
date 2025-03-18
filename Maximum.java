41792ad95adb0fbb43dfdb099043f33c43790fce
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-03-19 00:05:12 +0530

    initial commit

--- Maximum.java
+++ Maximum.java
@@ -2,6 +2,7 @@ public class Maximum {
     public static void main(String[] args) {
         int[] arr = { 1, 3, 44, 556, 76 };
         System.out.println(max(arr));
+        System.out.println(maxRange(arr, 1, 3));
     }
 
     static int max(int[] arr) {
@@ -14,15 +15,15 @@ public class Maximum {
         }
         return maxVal;
     }
-    // static int maxRange(int[] arr,int start,int end) {
-    // int maxVal = arr[start];
-    // for (int i = start; i < end; i++) {
-    // if (arr[i] > maxVal) {
-    // maxVal = arr[i];
-    // }
 
-    // }
-    // return maxVal;
-    // }
+    static int maxRange(int[] arr, int start, int end) {
+        int maxVal = arr[start];
+        for (int i = start; i < end; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
 
 }
