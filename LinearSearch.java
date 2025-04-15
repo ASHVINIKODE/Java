ea0264c56f2d2c2ab2f1f2d6bbb31ce8b2c0f036
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-04-15 20:27:10 +0530

    initial commit

--- LinearSearch.java
+++ LinearSearch.java
@@ -19,6 +19,8 @@ public class LinearSearch {
                 return index;
             }
         }
+        //this line will execute if none of the return statement above have executed
+        //hence the target not found
         return -1;
     }
 }
