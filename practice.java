ecca3f95e49ff38269441bd35d91d22e4e9775ec
ASHVINIKODE <155357703+ASHVINIKODE@users.noreply.github.com>
2025-06-22 20:45:33 +0530

    initial commit

---
+++ practice.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String[] args) {
+        // String name="Ashvii";
+        // int age=22;
+        // System.out.println("Name:"+name);
+        // System.out.println("Age:"+age);
+        // System.out.println("Name:"+name);
+        // System.out.println("Age:"+age);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the name:");
+        String Name = scan.nextLine();
+        System.out.println("Enter the Age:");
+        int Age = scan.nextInt();
+        System.out.println();
+        System.out.println("----------------------------------------");
+        System.out.println();
+        System.out.println("Name:" + Name);
+        System.out.println("Age:" + Age);
+        System.out.println("----------------------------------------");
+        System.out.println("Thank youu!!!");
+
+    }
+}
